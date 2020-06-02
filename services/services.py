9ec1b610cc9a498ed194c36f67152ba7149a5102
Ankush Chavan <cankush625@gmail.com>
2020-06-02 22:43:37 +0530

    AWS CloudTrail service added
    Signed-off-by: Ankush Chavan <cankush625@gmail.com>

--- services/services.py
+++ services/services.py
@@ -1,6 +1,7 @@
 import os
 from services.ec2.services_ec2 import ec2_services
 from services.IAM.iam_services import iam_services
+from services.CloudTrail.cloudtrail_services import cloudtrail_services
 
 def aws_services():
     while True:
@@ -10,7 +11,8 @@ def aws_services():
             Press
                 1. EC2
                 2. IAM
-                3. Exit
+                3. CloudTrail
+                4. Exit
                 ''')
         os.system("tput setaf 7")
 
@@ -22,4 +24,6 @@ def aws_services():
         if choice == 2:
             iam_services()
         if choice == 3:
+            cloudtrail_services()
+        if choice == 4:
             exit()
