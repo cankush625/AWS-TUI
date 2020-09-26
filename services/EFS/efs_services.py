bbe63553d753dcb6a95456c7f1c0979e59cdfa2f
cankush625 <cankush625@gmail.com>
2020-09-26 23:57:46 +0530

    Create EFS storage feature added
    Signed-off-by: cankush625 <cankush625@gmail.com>

---
+++ services/EFS/efs_services.py
@@ -0,0 +1,37 @@
+import os
+
+def efs_services():
+     while True:
+        os.system("tput setaf 4")
+        print('''
+        
+            Press
+                1. Create EFS Storage
+                2. List Storages
+                3. Get Storage
+                4. List Invalidations
+                5. Get Invalidation
+                6. Update Root Object
+                7. Delete Storage
+                8. Exit
+                ''')
+        os.system("tput setaf 7")
+
+        # Taking user choice to run the service
+        choice = int(input("Enter your choice: "))
+
+        if choice == 1:
+            createEFSStorage()
+        if choice == 8:
+            exit()
+
+# Creating new EFS storage
+def createEFSStorage():
+    creationToken = input("Enter the creation token: ")
+    performanceMode = input("Enter the performance mode: ")
+    throughputMode = input("Enter the throughtput mode: ")
+    tagName = input("Enter the tag name: ")
+    try:
+        os.system("aws efs create-file-system --creation-token {0} --performance-mode {1} --throughput-mode {2} --tags Key=Name,Value='{3}'".format(creationToken, performanceMode, throughputMode, tagName))
+    except FileSystemAlreadyExists:
+        print("File System already exist")
